5cc393aa335ebeefe1ff00e7aca920d27b4e6f57
William Suane <william.suane@devdojo.academy>
2023-05-11 20:30:52 +0200

    Unit test controller 02

--- src/main/java/academy/devdojo/controller/ProducerController.java
+++ src/main/java/academy/devdojo/controller/ProducerController.java
@@ -24,7 +24,7 @@ public class ProducerController {
     private final ProducerService producerService;
 
     @GetMapping
-    public ResponseEntity<List<ProducerGetResponse>> list(@RequestParam(required = false) String name) {
+    public ResponseEntity<List<ProducerGetResponse>> findAll(@RequestParam(required = false) String name) {
         log.info("Request received to list all producers, param name '{}'", name);
         var producers = producerService.findAll(name);
 
--- src/test/java/academy/devdojo/controller/ProducerControllerTest.java
+++ src/test/java/academy/devdojo/controller/ProducerControllerTest.java
@@ -10,11 +10,13 @@ import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.nio.file.Files;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -28,6 +30,8 @@ class ProducerControllerTest {
     private ProducerData producerData;
     private List<Producer> producers;
 
+    @Autowired
+    private ResourceLoader resourceLoader;
     @BeforeEach
     void init(){
         var ufotable = Producer.builder().id(1L).name("Ufotable").createdAt(LocalDateTime.now()).build();
@@ -41,10 +45,45 @@ class ProducerControllerTest {
     @DisplayName("findAll() returns a list with all producers")
     @Order(1)
     void findAll_ReturnsAllProducers_WhenSuccessful() throws Exception {
+        var response = readResourceFile("get-producer-null-name-200.json");
+
         mockMvc.perform(MockMvcRequestBuilders.get("/v1/producers"))
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.[0].id").value(1L));
+                .andExpect(MockMvcResultMatchers.content().json(response));
+
+    }
+
+    @Test
+    @DisplayName("findAll() returns a list with found producers when name is not null")
+    @Order(2)
+    void findAll_ReturnsFoundProducers_WhenNameIsPassedAndFound() throws Exception {
+        var name = "Ufotable";
+        var response = readResourceFile("get-producer-ufotable-name-200.json");
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/v1/producers").param("name",name))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json(response));
+
+    }
+
+    @Test
+    @DisplayName("findAll() returns an empty list when no producer is found by name")
+    @Order(3)
+    void findAll_ReturnsEmptyList_WhenNoNameIsFound() throws Exception {
+        var name = "x";
+        var response = readResourceFile("get-producer-x-name-200.json");
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/v1/producers").param("name",name))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json(response));
+    }
+
+    private String readResourceFile(String fileName) throws Exception {
+        var file = resourceLoader.getResource("classpath:%s".formatted(fileName)).getFile();
+        return new String(Files.readAllBytes(file.toPath()));
     }
 
 }
