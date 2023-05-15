dd4b3979803fde80685e39b0d5cd296d0f85c121
William Suane <william.suane@devdojo.academy>
2023-05-15 17:35:45 +0200

    Externalizing configuration pt 01

--- src/main/java/academy/devdojo/controller/AnimeController.java
+++ src/main/java/academy/devdojo/controller/AnimeController.java
@@ -24,7 +24,7 @@ public class AnimeController {
 
     @GetMapping
     public ResponseEntity<List<AnimeGetResponse>> list(@RequestParam(required = false) String name) {
-        log.info("Request received to list all animes, param name '{}'", name);
+        log.error("Request received to list all animes, param name '{}'", name);
 
         var animes = animeService.findAll(name);
 
