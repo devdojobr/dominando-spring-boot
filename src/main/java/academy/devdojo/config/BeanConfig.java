1a8a45f8d7005dfe67ccd8402c26b39def07b849
William Suane <william.suane@devdojo.academy>
2023-05-16 20:01:18 +0200

    Externalizing configuration pt 03

--- src/main/java/academy/devdojo/config/BeanConfig.java
+++ src/main/java/academy/devdojo/config/BeanConfig.java
@@ -3,6 +3,7 @@ package academy.devdojo.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import test.outside.Connection;
 
 @Configuration
@@ -15,11 +16,13 @@ public class BeanConfig {
     private String password;
     @Bean
 //    @Primary
+    @Profile("mysql")
     public Connection connectionMySql(){
         return new Connection(url, username, password);
     }
 
     @Bean(name = "mongoDB")
+    @Profile("mongo")
     public Connection connectionMongoDb(){
         return new Connection("localhost", "mongodb", "xxxx");
     }
