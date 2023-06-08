ae56a15979a6489f38326a5fed89634238105e65
William Suane <william.suane@devdojo.academy>
2023-06-08 19:14:26 +0200

    Testing repository with @DataJpaTest

---
+++ user-service/src/test/java/academy/devdojo/repository/UserRepositoryTest.java
@@ -0,0 +1,41 @@
+package academy.devdojo.repository;
+
+import academy.devdojo.commons.UserUtils;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.jdbc.Sql;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Import(UserUtils.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+//@Transactional(propagation = Propagation.NOT_SUPPORTED)
+class UserRepositoryTest {
+    @Autowired
+    private UserRepository repository;
+    @Autowired
+    private UserUtils userUtils;
+
+    @Test
+    @DisplayName("save() creates an user")
+    @Order(1)
+    void save_CreatesUser_WhenSuccessful() {
+        var userToSave = userUtils.newUserToSave();
+        var user = repository.save(userToSave);
+        Assertions.assertThat(user).hasNoNullFieldsOrProperties();
+    }
+
+    @Test
+    @DisplayName("findAll() returns a list with all users")
+    @Order(2)
+    @Sql("/sql/init_one_user.sql")
+    void findAll_ReturnsAllUsers_WhenSuccessful() {
+        var users = repository.findAll();
+        Assertions.assertThat(users).isNotEmpty();
+    }
+
+}
---
+++ user-service/src/test/resources/sql/init_one_user.sql
@@ -0,0 +1,1 @@
+INSERT INTO "user" (first_name,last_name, email) values ('goku','son','goku.son@dbz.com');
