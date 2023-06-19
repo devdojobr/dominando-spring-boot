cb89edab2300f8bf0ec1c4907c512adf7d8934dd
William Suane <william.suane@devdojo.academy>
2023-06-19 17:54:11 +0200

    user profile jpql

--- user-service/src/main/java/academy/devdojo/repository/UserProfileRepository.java
+++ user-service/src/main/java/academy/devdojo/repository/UserProfileRepository.java
@@ -3,9 +3,14 @@ package academy.devdojo.repository;
 import academy.devdojo.domain.Profile;
 import academy.devdojo.domain.UserProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserProfileRepository extends JpaRepository<UserProfile, Long> {
 
+    @Query("SELECT up from UserProfile up join fetch up.user u join fetch up.profile p")
+    List<UserProfile> retrieveAll();
 }
