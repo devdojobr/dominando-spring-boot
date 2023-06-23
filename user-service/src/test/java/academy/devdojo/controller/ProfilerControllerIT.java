8fa4b5080fb36e74d8e4978719e7553dd08700d9
William Suane <william.suane@devdojo.academy>
2023-06-23 19:29:28 +0200

    integration tests 02

--- user-service/src/test/java/academy/devdojo/controller/ProfilerControllerIT.java
+++ user-service/src/test/java/academy/devdojo/controller/ProfilerControllerIT.java
@@ -1,25 +1,31 @@
 package academy.devdojo.controller;
 
+import academy.devdojo.commons.ProfileUtils;
 import academy.devdojo.response.ProfileGetResponse;
-import org.assertj.core.api.Assertions;
+import academy.devdojo.response.ProfilePostResponse;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.test.context.jdbc.Sql;
 
 import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProfilerControllerIT {
     private static final String URL = "/v1/profiles";
     @Autowired
     private TestRestTemplate testRestTemplate;
+    @Autowired
+    private ProfileUtils profileUtils;
 
     @Test
     @DisplayName("findAll() returns a list with all profiles")
@@ -28,9 +34,33 @@ class ProfilerControllerIT {
         var typeReference = new ParameterizedTypeReference<List<ProfileGetResponse>>() {};
         var response = testRestTemplate.exchange(URL, HttpMethod.GET, null, typeReference);
 
-        Assertions.assertThat(response).isNotNull();
-        Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-        Assertions.assertThat(response.getBody()).isNotNull().doesNotContainNull();
-        response.getBody().forEach(profileGetResponse -> Assertions.assertThat(profileGetResponse).hasNoNullFieldsOrProperties());
+        assertThat(response).isNotNull();
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isNotNull().doesNotContainNull();
+        response.getBody().forEach(profileGetResponse -> assertThat(profileGetResponse).hasNoNullFieldsOrProperties());
+    }
+
+    @Test
+    @DisplayName("findAll() returns an empty list when no profiles are found")
+    @Order(2)
+    void findAll_ReturnsEmptyList_WhenNoProfilesAreFound() throws Exception {
+        var typeReference = new ParameterizedTypeReference<List<ProfileGetResponse>>() {};
+        var response = testRestTemplate.exchange(URL, HttpMethod.GET, null, typeReference);
+
+        assertThat(response).isNotNull();
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isNotNull().isEmpty();
+    }
+
+    @Test
+    @DisplayName("save() creates profile")
+    @Order(3)
+    void save_CreatesProfile_WhenSuccessful() throws Exception {
+        var profileToSave = profileUtils.newProfileToSave();
+        var response = testRestTemplate.exchange(URL, HttpMethod.POST, new HttpEntity<>(profileToSave), ProfilePostResponse.class);
+
+        assertThat(response).isNotNull();
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        assertThat(response.getBody()).isNotNull().hasNoNullFieldsOrProperties();
     }
 }
