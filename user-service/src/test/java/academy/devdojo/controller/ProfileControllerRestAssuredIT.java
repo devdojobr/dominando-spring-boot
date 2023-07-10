ee56b2e19e9a08f74e07b24a884e138dddaab29c
William Suane <william.suane@devdojo.academy>
2023-07-10 13:29:08 -0400

    rest assured test 04

--- user-service/src/test/java/academy/devdojo/controller/ProfileControllerRestAssuredIT.java
+++ user-service/src/test/java/academy/devdojo/controller/ProfileControllerRestAssuredIT.java
@@ -6,11 +6,16 @@ import academy.devdojo.config.IntegrationTestContainers;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import net.javacrumbs.jsonunit.assertj.JsonAssertions;
+import net.javacrumbs.jsonunit.core.Option;
+import org.assertj.core.api.Assertions;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.ArgumentMatchers;
 import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,7 +28,11 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProfileControllerRestAssuredIT extends IntegrationTestContainers {
@@ -78,6 +87,7 @@ class ProfileControllerRestAssuredIT extends IntegrationTestContainers {
         var expectedResponse = fileUtils.readResourceFile("profile/post-response-profile-201.json");
 
         var response = RestAssured.given().contentType(ContentType.JSON).accept(ContentType.JSON)
+                .log().all()
                 .body(request)
                 .when()
                 .post(URL)
@@ -97,4 +107,41 @@ class ProfileControllerRestAssuredIT extends IntegrationTestContainers {
 
     }
 
+    @ParameterizedTest
+    @MethodSource("postProfileBadRequestSource")
+    @DisplayName("save() returns bad request when fields are invalid")
+    @Order(4)
+    void save_ReturnsBadRequest_WhenFieldsAreInvalid(String requestFileName, String responseFileName) throws Exception {
+        var request = fileUtils.readResourceFile("profile/%s".formatted(requestFileName));
+        var expectedResponse = fileUtils.readResourceFile("profile/%s".formatted(responseFileName));
+
+        var response = RestAssured.given().contentType(ContentType.JSON).accept(ContentType.JSON)
+                .log().all()
+                .body(request)
+                .when()
+                .post(URL)
+                .then()
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .log().all()
+                .extract().response().body().asString();
+
+        JsonAssertions.assertThatJson(response)
+                .node("timestamp")
+                .asString()
+                .isNotEmpty();
+
+        JsonAssertions.assertThatJson(response)
+                .whenIgnoringPaths("timestamp")
+                .when(Option.IGNORING_ARRAY_ORDER)
+                .isEqualTo(expectedResponse);
+
+    }
+
+    private static Stream<Arguments> postProfileBadRequestSource() {
+
+        return Stream.of(
+                Arguments.of("post-request-profile-blank-fields-400.json", "post-response-profile-blank-fields-400.json"),
+                Arguments.of("post-request-profile-empty-fields-400.json", "post-response-profile-empty-fields-400.json")
+        );
+    }
 }
