a727ff1ab7602a728bddce8bce6023c17fffef94
William Suane <william.suane@devdojo.academy>
2023-07-10 12:51:07 -0400

    rest assured test 03

--- user-service/src/test/java/academy/devdojo/controller/ProfileControllerRestAssuredIT.java
+++ user-service/src/test/java/academy/devdojo/controller/ProfileControllerRestAssuredIT.java
@@ -5,15 +5,25 @@ import academy.devdojo.commons.ProfileUtils;
 import academy.devdojo.config.IntegrationTestContainers;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import net.javacrumbs.jsonunit.assertj.JsonAssertions;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.Collections;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProfileControllerRestAssuredIT extends IntegrationTestContainers {
@@ -44,4 +54,47 @@ class ProfileControllerRestAssuredIT extends IntegrationTestContainers {
                 .statusCode(HttpStatus.OK.value())
                 .body(Matchers.equalTo(response));
     }
+
+    @Test
+    @DisplayName("findAll() returns an empty list when no profiles are found")
+    @Order(2)
+    void findAll_ReturnsEmptyList_WhenNoProfilesAreFound() throws Exception {
+        var response = fileUtils.readResourceFile("profile/get-all-profiles-empty-list-200.json");
+
+        RestAssured.given().contentType(ContentType.JSON).accept(ContentType.JSON)
+                .when()
+                .get(URL)
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .body(Matchers.equalTo(response))
+                .log().all();
+    }
+
+    @Test
+    @DisplayName("save() creates profile")
+    @Order(3)
+    void save_CreatesProfile_WhenSuccessful() throws Exception {
+        var request = fileUtils.readResourceFile("profile/post-request-profile-200.json");
+        var expectedResponse = fileUtils.readResourceFile("profile/post-response-profile-201.json");
+
+        var response = RestAssured.given().contentType(ContentType.JSON).accept(ContentType.JSON)
+                .body(request)
+                .when()
+                .post(URL)
+                .then()
+                .statusCode(HttpStatus.CREATED.value())
+                .log().all()
+                .extract().response().body().asString();
+
+        JsonAssertions.assertThatJson(response)
+                .node("id")
+                .asNumber()
+                .isPositive();
+
+        JsonAssertions.assertThatJson(response)
+                .whenIgnoringPaths("id")
+                .isEqualTo(expectedResponse);
+
+    }
+
 }
